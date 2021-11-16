01937c070f4d23fabe0c2c96d51853f5f86a45d6
ConroSatchmo <conradonegrin@gmail.com>
2021-11-16 09:43:36 -0300

    responsive corregido

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require("path");
 const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log("El servidor corriendo en el puerto 3000");
@@ -14,4 +16,22 @@ app.listen(3000, () => {
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+app.post("/register", (req, res) => {
+  console.log(req.body);
+
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });
