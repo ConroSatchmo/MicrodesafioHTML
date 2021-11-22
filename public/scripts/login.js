2fab4f71c509cbd354608af1d9f13c7c685d92f9
ConroSatchmo <conradonegrin@gmail.com>
2021-11-22 12:49:41 -0300

    con validacion de password

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,7 +1,12 @@
 function validateForm() {
   const userName = document.forms["myForm"]["userName"].value;
+  const userPassword = document.forms["myForm"]["userPassword"].value;
   if (userName == "") {
     alert("Debes completar el nombre");
+    return false;
+  }
+  if (userPassword == "") {
+    alert("Debes completar la contraseña");
     return false;
   }
 }
